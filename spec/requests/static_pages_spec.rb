2cf71eab5b05142d8780afb51b59637cbfde259e
yuuyy <yuuyy0001@gmail.com>
2015-01-24 23:21:13 +0900

    Answer ex-3.5.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,4 +40,18 @@ describe "Static pages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact page" do
+
+    # it "should have the content 'About Us'" do
+    #   visit '/static_pages/about'
+    #   expect(page).to have_content('About Us')
+    # end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
 end
